230976572338096b6f0814a23ae95c707925dbd3
Jake Chang <>
2024-04-19 13:23:33 +0800

    - Add

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,132 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mx-auto">
+    <div class="flex justify-center mt-4 gap-2">
+      <input type="text" placeholder="今天要做什麼？" class="input input-bordered input-md w-full max-w-xs" v-model="text" />
+      <button class="btn" @click="save" :disabled="isSave || text === ''">
+        <span class="loading loading-spinner loading-xs" v-if="isSave"></span>
+        儲存
+      </button>
+    </div>
+
+    <div class="overflow-x-auto">
+      <table class="table table-zebra">
+        <thead>
+          <tr>
+            <th>編號</th>
+            <th>內容</th>
+            <th>#</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, index) in items" :key="index">
+            <th>{{ index + 1 }}</th>
+            <td>
+              <p v-if="!item.isEdit">{{ item.text }}</p>
+              <input type="text" placeholder="今天要做什麼？" class="input input-bordered input-md w-full max-w-xs"
+                v-model="item.text" v-else />
+            </td>
+            <td>
+              <button class="btn btn-xs" @click="changeMode(index)" v-if="!item.isEdit">編輯</button>
+              <div class="flex gap-2" v-if="item.isEdit">
+                <button class="btn btn-success btn-outline btn-xs" @click="update(index)">更新</button>
+                <button class="btn btn-error btn-outline btn-xs" @click="remove(index)">刪除</button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted } from 'vue';
+import { Parse } from 'parse/dist/parse.min.js';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  setup() {
+    const text = ref('');
+    const isSave = ref(false);
+    const items = ref([]);
+
+    async function getData() {
+      try {
+        const query = new Parse.Query("ToDo");
+        let datas = await query.find();
+
+        items.value = [];
+        datas.forEach(data => {
+          items.value.push({
+            "id": data.id,
+            "text": data.get('text'),
+          })
+        });
+      }
+      catch (error) {
+        console.log(error);
+      }
+    }
+
+    async function save() {
+      isSave.value = true;
+
+      const toDoList = new Parse.Object("ToDo");
+      toDoList.set("text", text.value);
+      try {
+        await toDoList.save()
+        text.value = "";
+        await getData();
+        isSave.value = false;
+      }
+      catch (error) {
+        alert('上傳失敗 ' + error.message);
+      }
+    }
+
+    function changeMode(index) {
+      items.value[index].isEdit = true;
+    }
+
+    async function update(index) {
+      let item = items.value[index];
+
+      try {
+        const query = new Parse.Query("ToDo");
+
+        query.equalTo("objectId", item.id);
+        const toDo = await query.first();
+        toDo.set('text', item.text);
+        await toDo.save()
+        items.value[index].isEdit = false;
+      }
+      catch (error) {
+        alert('更新失敗 ' + error.message);
+      }
+    }
+
+    async function remove(index) {
+      let item = items.value[index];
+
+      try {
+        const query = new Parse.Query("ToDo");
+        query.equalTo("objectId", item.id);
+        const toDo = await query.first();
+        await toDo.destroy();
+        await getData();
+      }
+      catch (error) {
+        console.log(error);
+      }
+    }
+
+    onMounted(() => {
+      getData();
+    });
+
+    return {
+      text, isSave, items,
+      save, changeMode, update, remove,
+    }
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
